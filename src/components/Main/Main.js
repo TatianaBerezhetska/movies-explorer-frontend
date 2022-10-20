f38dd57d5eb73ac0e2ae9c92c0c187440167eb8a
Tatiana Berezhetskaya <t.berezhetskaya@gmail.com>
2022-10-20 16:00:01 +1000

    hotfixes in Main component structure

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -11,11 +11,13 @@ function Main() {
 
   return (
     <>
-      <Promo />
-      <AboutProject />
-      <Techs />
-      <AboutMe />
-      <Portfolio />
+      <main>
+        <Promo />
+        <AboutProject />
+        <Techs />
+        <AboutMe />
+        <Portfolio />
+      </main>
       <Footer />
     </>
   );
